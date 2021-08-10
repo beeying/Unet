1cdefd425254d66b62df15682188e299176fca9d
beeying <37954607+beeying@users.noreply.github.com>
2021-08-10 13:50:08 +0700

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+from .autoencoder import *
+from .unet import *
+from .anogan import *
+from .vae import *
+from .deeplab_v3 import deeplabv3_resnet50 as deeplabv3
+from .gan import GAN
+from .massl import MASSL
